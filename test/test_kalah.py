f376f5fed3919772ac3e9479a8ce2166f1a5872b
Jay Harris <jaywritescode@users.noreply.github.com>
2022-04-21 22:58:18 -0400

    test player two ends in their own empty house & player sows around the board

--- test/test_kalah.py
+++ test/test_kalah.py
@@ -30,10 +30,34 @@ def after_six_plies():
     The board after 1. c, f c 2. e a, d 3. f, d a
     """
     k = Kalah()
-    for (seedable, count) in zip(k.board,  [3, 6, 5, 1, 0, 1, 1, 5, 1, 8, 3, 0, 7, 7]):
+    for (seedable, count) in zip(k.board, [3, 6, 5, 1, 0, 1, 1, 5, 1, 8, 3, 0, 7, 7]):
         seedable.count = count
     consume(k.players, 6)
     return k
+
+@pytest.fixture
+def after_seven_plies():
+    """
+    The board after 1. d a 2. a c 3. e c 4. c ...
+    If player two plays at a, they will end in an empty house.
+    """
+    k = Kalah()
+    for (seedable, count) in zip(k.board, [1, 1, 5, 0, 2, 1, 7, 3, 2, 6, 0, 8, 6, 6]):
+        seedable.count = count
+    consume(k.players, 7)
+    return k
+
+@pytest.fixture
+def after_nine_plies():
+    """
+    The board after 1. e e 2. c, b, f d 3. c b 4. b a, b 5. d ...
+    """
+    k = Kalah()
+    for (seedable, count) in zip(k.board, [4, 7, 0, 0, 0, 3, 1, 7, 1, 1, 9, 2, 4, 9]):
+        seedable.count = count
+    consume(k.players, 9)
+    return k
+
 
 
 def test_it_plays_one_seed_in_the_next_n_houses(initial_state):
@@ -47,7 +71,6 @@ def test_it_plays_one_seed_in_the_next_n_houses(initial_state):
     kalah.next()
     assert_game(kalah, [0, 0, 5, 5, 5, 5, 4, 0, 4, 4, 4, 4, 4, 4])
 
-
 def test_it_plays_seeds_in_the_opponents_houses(initial_state):
     kalah = initial_state
     player = kalah.player1
@@ -59,7 +82,6 @@ def test_it_plays_seeds_in_the_opponents_houses(initial_state):
     kalah.next()
     assert_game(kalah, [0, 4, 4, 4, 4, 4, 0, 1, 5, 5, 5, 4, 4, 4])
 
-
 def test_it_ends_in_the_players_store(initial_state):
     kalah = initial_state
     player = kalah.player1
@@ -70,7 +92,6 @@ def test_it_ends_in_the_players_store(initial_state):
 
     kalah.next()
     assert_game(kalah, [0, 4, 4, 0, 5, 0, 6, 2, 5, 5, 5, 4, 4, 4])
-
 
 def test_player_two_can_move(after_one_ply):
     kalah = after_one_ply
@@ -94,11 +115,27 @@ def test_player_one_ends_in_their_own_empty_house(after_six_plies):
     kalah.next()
     assert_game(kalah, [3, 6, 5, 0, 0, 1, 1, 9, 1, 8, 0, 0, 7, 7])
 
-def test_player_two_ends_in_their_own_empty_house():
-    pass
+def test_player_two_ends_in_their_own_empty_house(after_seven_plies):
+    kalah = after_seven_plies
+    player = kalah.player2
 
-def test_player_sows_around_the_entire_board():
-    pass
+    player.get_move = mock.MagicMock(
+        name="get_move", return_value=Move(player, player.houses[0])
+    )
+
+    kalah.next()
+    assert_game(kalah, [4, 1, 5, 0, 0, 1, 7, 3, 0, 7, 0, 8, 6, 6])
+
+def test_player_sows_around_the_entire_board(after_nine_plies):
+    kalah = after_nine_plies
+    player = kalah.player2
+
+    player.get_move = mock.MagicMock(
+        name="get_move", return_value=Move(player, player.houses[5])
+    )
+
+    kalah.next()
+    assert_game(kalah, [5, 8, 1, 1, 1, 4, 2, 7, 2, 2, 9, 2, 4, 0])
 
 
 def assert_game(game, board):
